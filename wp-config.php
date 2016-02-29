419d564f820e467409d3500f1c158c928ceb9b1a
Alexander Dolgan <5704737@gmail.com>
2016-02-29 11:57:14 +0300

    last

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'sputnik');
+define('DB_NAME', '449115_wp1');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', '449115_wp1');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'cTQ4hn66z');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'z9496.spectrum.myjino.ru');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
